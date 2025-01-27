30a1cb0a89655f3a5c505488133662f12db4d909
Arthurdev02 <arthur.cev.taf@gmail.com>
2025-01-27 16:38:16 +0100

    [A] de différent information sur les classes

--- app/php-vs-js.js
+++ app/php-vs-js.js
@@ -1,14 +1,29 @@
 console.log('Hello !');
 
 // La classe 
-class Character {
+class Character { 
+    // Déclaration de propriétés statiques
+    static counter = 0;
+
     //Déclaration de propriétés publiques
     hp;
     armor;
     luck;
+ // Déclaration d'une méthode statique 
+ static getCounter() {
+// dans uen méthode statique, "this" est équivalent à " self" en PHP. 
+//Dans le contexte d'une méthode statique, "this" fait référence à la classe 
+//retun this.counter;
+//On peut aussi utiliser ce type d'apelle qui est identique à celui du contexte non statique
+// pour des raison de lisibilité on privilégie cette forme. 
+ return Character.counter;
+}
 
     //Cnnstructeur
     constructor(jsonCharacter) {
+        //Dans le contexte d'une méthode statique, "this" fait référence à l'instance actuelle" 
+        //Appel d'un propriété statique dans un contexte non statique
+        Character.counter ++;
         this.hp = jsonCharacter.hp;
         this.armor = jsonCharacter.armor;
         this.luck = jsonCharacter.luck;
